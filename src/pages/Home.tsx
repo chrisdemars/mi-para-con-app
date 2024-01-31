0ebc85526dd63a0996c13e230ad40d76930b83d4
Chris DeMars <demars@Chriss-MacBook-Pro-2.local>
2024-01-31 14:34:54 -0500

    adds valley camp stuff

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,41 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCardContent } from '@ionic/react';
+import { IonContent, IonPage, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonImg } from '@ionic/react';
 
 const Home: React.FC = () => {
   return (
     <IonPage>
       <IonContent fullscreen>
-        {/* <IonHeader>
-          <IonToolbar>
-            <IonTitle size="large">MI ParaCon</IonTitle>
-          </IonToolbar>
-        </IonHeader> */}
         <IonCard>
-          <img alt="Silhouette of mountains" src="../public/hero-image.webp" />
+          <IonImg alt="Silhouette of mountains" src="../public/hero-image.webp" ></IonImg>
           <IonCardHeader>
             <IonCardTitle>👻 Welcome!</IonCardTitle>
             {/* <IonCardSubtitle>Michigan ParaCon</IonCardSubtitle> */}
           </IonCardHeader>
           <IonCardContent>The 2024 Michigan Paranormal Convention will bring the biggest names in the paranormal industry to Sault Ste. Marie, MI for the 14th time.</IonCardContent>
-          <IonCardContent>Between August 22 and 24, 2024 in Sault Ste. Marie's Kewadin Casino's DreamMakers Theater, experts and television stars will be on hand to cover such topics as paranormal investigating, psychic powers, demonology, and UFOlogy. Come see the biggest names speak on today's hottest topic, the paranormal!</IonCardContent>
+          <IonCardContent>
+            Between August 22 and 24, 2024 in Sault Ste. Marie's Kewadin Casino's DreamMakers Theater, experts and television stars will be on hand to cover such topics as paranormal investigating, psychic powers, demonology, and UFOlogy. Come see the biggest names speak on today's hottest topic, the paranormal!
+          </IonCardContent>
+        </IonCard>
+        <h1>👻 Special Events!</h1>
+        <IonCard>
+          <IonImg alt="Silhouette of mountains" src="../public/valley-camp.webp"></IonImg>
+          <IonCardHeader>
+            <IonCardTitle>VALLEY CAMP INVESTIGATION</IonCardTitle>
+          </IonCardHeader>
+          <IonCardContent>Date: Thursday, Aug. 22, 2024
+            Time: 8 p.m. – Midnight
+            Price: $120.00 each
+            Seating Capacity: 100
+
+            We’re bringing back our ever popular Paranormal Investigation to the 106-year-old Museum Ship Valley Camp. Join paranormal celebs and experts on this investigation! This experience is limited to 100 people and the group will be broken into smaller investigation stations. Participants must provide their own transportation to the Valley Camp.</IonCardContent>
+        </IonCard>
+        <IonCard>
+          <IonCardHeader>
+            <IonCardTitle>📋 Workshops!</IonCardTitle>
+          </IonCardHeader>
+          <IonCardContent>The 2024 Michigan Paranormal Convention will bring the biggest names in the paranormal industry to Sault Ste. Marie, MI for the 14th time.</IonCardContent>
+          <IonCardContent>
+            Between August 22 and 24, 2024 in Sault Ste. Marie's Kewadin Casino's DreamMakers Theater, experts and television stars will be on hand to cover such topics as paranormal investigating, psychic powers, demonology, and UFOlogy. Come see the biggest names speak on today's hottest topic, the paranormal!
+          </IonCardContent>
         </IonCard>
       </IonContent>
     </IonPage>
